fbfbf129507ff176090f8e619209954d186b83ac
Noah Ivers <noahivers@Noahs-MacBook-Pro.local>
2026-03-01 17:44:47 -0500

    new theme and map added

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import "../styles/main.css"
-import headshot from "../assets/headshot.jpeg";
+import '../styles/main.css';
+import headshot from '../assets/headshot.jpeg';
+import JourneyMap from './JourneyMap';
 
 function Main() {
-
-	return (
-		<div>
-            <div className="openingContainer">
-                    <div className="openingText">
-                        <h1 className='introHeader'>
-                            I'm Noah Ivers
-                        </h1>
-                        <p className='introPara'>
-                        An aspiring Full-Stack Developer with experience building web applications for small business' and individual clients
-                        </p>
-                    </div>
-                <div className="openingPicture">
-                <img className="headshotPhoto" src={headshot} alt="Noah Ivers Headshot"/>
-                </div>
-            </div>
-		</div>
-	);
+  return (
+    <div>
+      <div className="openingContainer">
+        <div className="openingText">
+          <h1 className="introHeader">
+            I&apos;m Noah Ivers
+          </h1>
+          <p className="introPara">
+            A Full-Stack Developer with experience building applications for individual clients, small businesses, and enterprise organizations.          </p>
+        </div>
+        <div className="openingPicture">
+          <img className="headshotPhoto" src={headshot} alt="Noah Ivers Headshot" />
+        </div>
+      </div>
+      <JourneyMap />
+    </div>
+  );
 }
 
-export default Main
+export default Main;
