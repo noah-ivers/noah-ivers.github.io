ec512a92bcda1b807795ad2648f47f4a82d045cd
Noah Ivers <noahivers@Noahs-MacBook-Pro.local>
2026-03-01 21:24:34 -0500

    fixed error

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -15,29 +15,29 @@ function Skills() {
                     <h3 className='stackh2'>
                         My Tech Stack
                     </h3>
-                        <ul className='stackListContainer'>
-                            <li className='stacklist'>
-                                Building full-stack web apps with TypeScript/JavaScript (Angular, React) and Java (Spring Boot), plus Python (Flask, Django)
-                            </li>
-                            <li className='stacklist'>
-                                Designing and shipping cloud features on Google Cloud Platform, including Firestore-backed APIs and secure deployments
-                            </li>
-                            <li className='stacklist'>
-                                Quality + security focused development with Git, Jasmine unit tests, SonarQube, and 42Crunch
-                            </li>
-                            <li className='stacklist'>
-                                Data visualization and mapping interfaces using MapLibre and Three.js, plus SQL for data access
-                            </li>
-                        </ul>
+                    <ul className='stackListContainer'>
+                        <li className='stacklist'>
+                            Building full-stack web apps with TypeScript/JavaScript (Angular, React) and Java (Spring Boot), plus Python (Flask, Django)
+                        </li>
+                        <li className='stacklist'>
+                            Designing and shipping cloud features on Google Cloud Platform, including Firestore-backed APIs and secure deployments
+                        </li>
+                        <li className='stacklist'>
+                            Quality + security focused development with Git, Jasmine unit tests, SonarQube, and 42Crunch
+                        </li>
+                        <li className='stacklist'>
+                            Data visualization and mapping interfaces using MapLibre and Three.js, plus SQL for data access
+                        </li>
+                    </ul>
                 </div>
                 <div className='stackContainer'>
-                        {mytechstack.languages.map((languages, i) => {
+                    {mytechstack.languages.map((languages, i) => {
                         return (
                             <div className='stack' key={languages.language}>
-                                <img className='logos' src={languages.picture} alt='Programming Language Icon'/>
+                                <img className='logos' src={languages.picture} alt='Programming Language Icon' />
                                 <p className='stackText'>{languages.language}</p>
                             </div>
-                    );
+                        );
                     })}
                 </div>
             </div>
@@ -47,7 +47,7 @@ function Skills() {
                 </h3>
                 <div className='eduContainer'>
                     <div className='eduPic'>
-                        <img className='umichPic' src={umich} alt='University of Michigan Seal'/>
+                        <img className='umichPic' src={umich} alt='University of Michigan Seal' />
                     </div>
                     <div className='eduText'>
                         <h4 className='umichh4'> University of Michigan </h4>
@@ -59,7 +59,7 @@ function Skills() {
                                 Bachelor of Science in Computer Science
                             </li>
                             <li className='umichBullet'>
-                                Coursework: Data Structures and Algorithms, Web Systems, Database Management Systems, 
+                                Coursework: Data Structures and Algorithms, Web Systems, Database Management Systems,
                                 Software Engineering, Multivariable And Vector Calculus, Discrete Mathematics, Computer Organization
                             </li>
                         </ul>
